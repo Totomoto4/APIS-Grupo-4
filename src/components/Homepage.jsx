714237a1755ef2def4dcb0fa2e7653d27feb0e2d
Thomas Imbriago <thomas.imbriago29@gmail.com>
2024-04-17 11:28:33 -0300

    Agregado del Router, dummys y logo

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+import logo from '../imagenes/logo.svg';
+
+export default function Homepage(){
+
+    return(
+        <section>
+            <h1>PRUEBA HOMEPAGE</h1>
+            <img src={logo} alt='logo empresa'/>
+        </section>
+    )
+}
