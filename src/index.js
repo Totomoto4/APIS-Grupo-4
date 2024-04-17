9b49546739a42582842f9cc28c8f1da1f12132dc
Francisco Zanon <panchizanon@gmail.com>
2024-04-17 10:05:53 -0300

    Login y Register

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
+import HomePage from './HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +17,7 @@ root.render(
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<App />} />
+        <Route path="/homepage" element={<HomePage />} />
       </Routes>
     </Router>
   </React.StrictMode>
