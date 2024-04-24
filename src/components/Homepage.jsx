6d843b176646910b3b68c437894ef09763452223
Francisco Zanon <panchizanon@gmail.com>
2024-04-24 00:54:18 -0300

    Header and Cards v1.0

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,19 +1,34 @@
+import React from 'react';
 import { useContext } from 'react';
-
 import { UserContext } from '../context/UserContext.tsx';
-
+import Header from './Header.jsx';
+import ProductCard from './ProductCard.jsx'; // Importa el componente de tarjeta de productos
 import logo from '../imagenes/logo.svg';
 import { Link } from 'react-router-dom';
+import './Homepage.css';
+import { products } from '../dummys/products.js'
+
+
 
 export default function Homepage(){
 
-    const {user, updateUser} = useContext(UserContext);
+    const { user, updateUser } = useContext(UserContext);
     console.log(user);
 
     return(
         <section>
-            <h1>PRUEBA HOMEPAGE</h1>
-            {user ? <p>Usuario logueado: {user.nombreUsuario}</p> :<Link to='/login'>Log in!</Link>}
+            <Header />
+            <div className="content">
+                <h1>PRUEBA HOMEPAGE</h1>
+                {user ? <p>Usuario logueado: {user.nombreUsuario}</p> : <Link to='/login'>Log in!</Link>}
+                
+                {/* Renderiza las tarjetas de productos */}
+                <div className="product-cards">
+                    {products.map(product => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+            </div>
         </section>
     )
 }
