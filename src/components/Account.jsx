6b9dace1a52064ca3bba6f99f208d37f066e25b3
PerezzL <lucasperezmarquez1@gmail.com>
2024-05-07 15:43:41 -0300

    Account upgrade

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from './redux/actions';
 import './Account.css';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext.tsx';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
 
 const Account = () => {
-  const usuario = useSelector(state => state.usuario);
-  const isAdmin = useSelector(state => state.isAdmin);
-  const dispatch = useDispatch();
-  
+  const { user, updateUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const isAdmin = user && user.rol === 'admin';
+
   const handleSignOut = () => {
-    dispatch(logout());
+    navigate('/login')
   };
 
   return (
     <div className="dropdownAc">
       <button className="dropbtnAc">Cuenta</button>
       <div className="dropdown-content-Ac">
-        <h4>{usuario ? usuario.nombre : 'Usuario Invitado'}</h4>
+        <h4>{user ? user.nombreUsuario : 'Usuario Invitado'}</h4>
         <button className='config'>Configuraciones</button>
         {isAdmin && <button className='BtnAdmin'>Publicaciones</button>}
-        <button className='signOut' onClick={handleSignOut}>Cerrar Sesión</button>
+        <button className='signOut' onClick={ handleSignOut }>Cerrar Sesión</button>
       </div>
     </div>
   );
