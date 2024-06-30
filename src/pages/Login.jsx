a1a0778fbbaf92d4aff2bbb0a23c86b2c2725b00
Juan Schelotto <68937574+MACHIXS@users.noreply.github.com>
2024-06-30 12:27:28 -0300

    Eliminacion Comments
    Eliminacion Comments en registro y login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,86 +1,3 @@
-/* 
-import React, { useContext, useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import './Login.css';
-
-import { USUARIOS } from '../dummys/usuariosDummy.js';
-import { UserContext } from '../context/UserContext.tsx';
-import { useDispatch } from 'react-redux';
-import logo from '../imagenes/generales/kiwi-logo-pequeño.png';
-
-//Esta funcionalidad sera del backend, es temporal.
-function authUser(email, password){
-  //valida el email y contraseña, si son valido devuelve el usuario, sino undefined
-  const usuarioEncontrado = USUARIOS.find( (usuario) => usuario.email === email.trim());
-
-  if(usuarioEncontrado && usuarioEncontrado.contraseña === password){
-    return usuarioEncontrado;
-  }
-  
-  return undefined; //si no lo encontro o la password esta mal, devuelve undefined
-}
-
-const Login = () => {
-
-  //State del form
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aquí puedes agregar la lógica para procesar el inicio de sesión
-    console.log('Correo electrónico:', email);
-    console.log('Contraseña:', password);
-
-    const usuarioEncontrado = authUser(email, password);
-    if (usuarioEncontrado) {
-      dispatch({ type: 'SET_USER', payload: usuarioEncontrado });
-      navigate('/home');
-    } else {
-      console.log('Los datos son incorrectos');
-    }
-  };
-
-  return (
-    <div className="login-container">
-      <div className="login-form">
-        <img src={logo} alt='logo' id='logo'/>
-        <h1>Inicio de sesión</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Correo electrónico
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value.trim())}
-            />
-          </label>
-          <label>
-            Password
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <button type="submit">Iniciar sesión</button>
-        </form>
-        <div className="login-footer">
-          <p>
-            ¿No tienes cuenta? <Link to="/register">Regístrate</Link>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Login;
-*/
-
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import './Login.css';
@@ -104,11 +21,9 @@ const Login = () => {
         try {
             const response = await axios.post('http://localhost:8080/auth/login', requestBody);
             console.log('Inicio de sesión exitoso:', response.data);
-            // Aquí puedes manejar la respuesta, guardar el token JWT en el localStorage, etc.
-            navigate('/home'); // Redirige a la página de inicio después del login exitoso
+            navigate('/home');
         } catch (error) {
             console.error('Error en el inicio de sesión:', error);
-            // Aquí puedes manejar el error, como mostrar un mensaje al usuario
         }
     };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,92 +1,9 @@
-/*import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import './Register.css';
-
-import logo from '../imagenes/generales/kiwi-logo-pequeño.png';
-
-//ESTA FUNCION SERA IMPLEMENTADA EN BACKEND
-function verificarDisponibilidad(email){
-}
-
-
-const Register = () => {
-  const [name, setName]= useState('');
-  const [lastName, setLastName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-  };
-
-  return (
-    <div className="register-container">
-      <div className="register-form">
-        <img src={logo} alt='logo' id='logo'/>
-        <h1>Registro</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Nombre:
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value.trim())}
-            />
-          </label>
-
-          <label>
-            Apellido:
-            <input
-              type="text"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value.trim())}
-            />
-          </label>
-
-
-          <label>
-            Correo electrónico:
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value.trim())}
-            />
-          </label>
-
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-
-          <button type="submit">Registrarse</button>
-        </form>
-        <div className="register-footer">
-          <p>
-            ¿Tienes una cuenta? <Link to="/login">Inicia Sesión</Link>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Register;
-
-*/
-
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import './Register.css';
 
 import logo from '../imagenes/generales/kiwi-logo-pequeño.png';
-import axios from 'axios'; // Asegúrate de importar Axios si no está importado
+import axios from 'axios'; 
 
 const Register = () => {
   const [name, setName] = useState('');
@@ -108,7 +25,7 @@ const Register = () => {
     try {
       const response = await axios.post('http://localhost:8080/auth/register', requestBody);
       console.log('Registro exitoso:', response.data);
-      navigate('/login'); // Redirigir a la página de inicio de sesión después del registro exitoso
+      navigate('/login'); 
     } catch (error) {
       console.error('Error en el registro:', error);
     }
