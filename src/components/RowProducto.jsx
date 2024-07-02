d455143f904654eb3fad3b3f70a585a723f35b2f
Juan Schelotto <68937574+MACHIXS@users.noreply.github.com>
2024-07-02 20:31:42 -0300

    Connection Admin Panel Front-Back
    Connection Front-Back
     Admin Panel Product listing data recovery from database

--- src/components/RowProducto.jsx
+++ src/components/RowProducto.jsx
@@ -1,18 +1,42 @@
-import { useState } from "react"
+import React from 'react';
 
-export default function RowProducto({producto, activarProducto}){
+function RowProducto({ producto, activarProducto }) {
 
-    function handleRowClick(){
-        activarProducto(producto)
+    function handleRowClick() {
+        activarProducto(producto);
     }
 
-    return(
-        <tr key={producto.id} className={`product-row ${producto.stock < 1 ? "sin-stock" : undefined}`} onClick={() => handleRowClick()}>
-                    <td><img src={producto.image} height='30'/></td>
-                    <td>{producto.id}</td>
-                    <td>{producto.name}</td>
-                    <td>{producto.price}</td>
-                    <td>{producto.stock}</td>
-                </tr>
-    )
+
+  const getImageSource = () => {
+    if (!producto.imageData || typeof producto.imageData !== 'string') {
+      return ''; // Manejar caso donde producto.image no está definido o no es una cadena válida
+    }
+
+    const base64Prefix = 'data:image/';
+    let imageExtension = 'png'; // Por defecto PNG si no se detecta extensión
+
+    // Detectar JPEG
+    if (producto.imageData.startsWith('/9j/')) {
+      imageExtension = 'jpeg';
+    }
+
+    // Detectar WebP
+    if (producto.imageData.startsWith('UklGR')) {
+      imageExtension = 'webp';
+    }
+
+    return `${base64Prefix}${imageExtension};base64, ${producto.imageData}`;
+  };
+
+  return (
+    <tr key={producto.id} className={`product-row ${producto.stock < 1 ? "sin-stock" : ""}`}onClick={handleRowClick}>
+      <td><img src={getImageSource()} height='30' alt={producto.name} /></td>
+      <td>{producto.id}</td>
+      <td>{producto.name}</td>
+      <td>{producto.price}</td>
+      <td>{producto.stock}</td>
+    </tr>
+  );
 }
+
+export default RowProducto;
--- src/components/TableProductos.jsx
+++ src/components/TableProductos.jsx
@@ -1,119 +1,157 @@
-import RowProducto from "./RowProducto"
-import {products} from "../dummys/productsSimpsons"
-
+import React, { useEffect, useState } from 'react';
+import RowProducto from "./RowProducto";
+import { fetchAllProducts } from '../funcionesFetch/productFunctions'; // Asegúrate de importar correctamente
 import './TableProductos.css';
 import NewProdModal from "./NewProdModal";
-import { useState } from "react";
-import { isDisabled } from "@testing-library/user-event/dist/utils";
 import { useNavigate } from 'react-router-dom';
 
-export default function TableProductos(){
-
+export default function TableProductos() {
+    const [productos, setProductos] = useState([]);
     const [productoActivo, setProductoActivo] = useState();
     const [isUnderChanges, setIsUnderChanges] = useState(false);
-    const [showModal ,setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const navigate = useNavigate();
 
-    function activarProducto(producto){
-      setProductoActivo(producto);
+    useEffect(() => {
+        async function getProducts() {
+            try {
+                const data = await fetchAllProducts();
+                setProductos(data);
+                console.log("Productos obtenidos:", data); // Verificar si los productos se obtienen correctamente
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        }
+        getProducts();
+    }, []);
+
+    function activarProducto(producto) {
+        setProductoActivo(producto);
     }
 
-    function desactivarProducto(){
-      setIsUnderChanges(false)
-      setProductoActivo(undefined);
+    function desactivarProducto() {
+        setIsUnderChanges(false);
+        setProductoActivo(undefined);
     }
 
     const handleAgregarProducto = () => {
-      setShowModal(true);
+        setShowModal(true);
     };
 
     const closeModal = () => {
-      setShowModal(false);
-    }
+        setShowModal(false);
+    };
 
     const handleEliminarProducto = () => {
-      if (productoActivo) {
-        const updatedProducts = products.filter(prod => prod.id !== productoActivo.id);
-        desactivarProducto();
-      }
-    }
+        if (productoActivo) {
+            const updatedProducts = productos.filter(prod => prod.id !== productoActivo.id);
+            setProductos(updatedProducts);
+            desactivarProducto();
+        }
+    };
 
-    function renderTable(){
-      return(
-        <table id="TABLE-GENERAL">
-          <thead>
-            <tr>
-              <th><button id='AGREGAR-PROD' onClick={handleAgregarProducto}>Agregar Producto</button></th>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Precio</th>
-              <th>Stock</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((producto) => (
-              <RowProducto producto={producto} activarProducto={activarProducto}></RowProducto>
-            ))}
-          </tbody>
-        </table>
-      )
-    }
-
-    function renderProducto(){
-      return (
-        <div className="admin-product-container">
-          <button onClick={desactivarProducto} id="boton-desactivador">
-            X
-          </button>
-          <div className="admin-image-container">
-            <img src={productoActivo.image} alt="imagen del producto" />
-          </div>
-
-          <section className="producto-info-admin">
-            <table id="TABLE-INFO">
-              <tr>
-                <td className="left-header">Nombre</td>
-                <td className="right-datacell">
-                  {isUnderChanges ? <input type="text"  defaultValue={productoActivo.name} /> : <p>{productoActivo.name}</p>}
-                </td>
-              </tr>
-              <tr>
-                <td className="left-header">Descripcion</td>
-                <td className="right-datacell">
-                {isUnderChanges ? <input type="text"  defaultValue={productoActivo.description} /> : <p>{productoActivo.description}</p>}
-                </td>
-              </tr>
-              <tr>
-                <td className="left-header">Precio</td>
-                <td className="right-datacell">
-                {isUnderChanges ? <input type="number"  defaultValue={productoActivo.price} /> : <p>{productoActivo.price}</p>}
-                </td>
-              </tr>
-              <tr>
-                <td className="left-header">Stock</td>
-                <td className="right-datacell">
-                {isUnderChanges ? <input type="number"  defaultValue={productoActivo.stock} /> : <p>{productoActivo.stock}</p>}
-                </td>
-              </tr>
+    function renderTable() {
+        return (
+            <table id="TABLE-GENERAL">
+                <thead>
+                    <tr>
+                        <th><button id='AGREGAR-PROD' onClick={handleAgregarProducto}>Agregar Producto</button></th>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {productos.map((producto) => (
+                        <RowProducto key={producto.id} producto={producto} activarProducto={activarProducto} />
+                    ))}
+                </tbody>
             </table>
-            <button id="ACEPTAR-BTN" onClick={handleAceptarBTN}>{isUnderChanges ? "Aceptar" : "Modificar"}</button>
-            <button id="ELIMINAR-BTN" onClick={handleEliminarProducto}>Eliminar Producto</button>
-          </section>
-        </div>
-      );
+        );
     }
 
-    function handleAceptarBTN(){
-      if(isUnderChanges){
-        setIsUnderChanges(false)
-      } else {
-        setIsUnderChanges(true)
-      }
+    function renderProducto() {
+
+      const getImageSource = () => {
+        if (!productoActivo.imageData || typeof productoActivo.imageData !== 'string') {
+          return ''; // Manejar caso donde productoActivo.image no está definido o no es una cadena válida
+        }
+    
+        const base64Prefix = 'data:image/';
+        let imageExtension = 'png'; // Por defecto PNG si no se detecta extensión
+    
+        // Detectar JPEG
+        if (productoActivo.imageData.startsWith('/9j/')) {
+          imageExtension = 'jpeg';
+        }
+    
+        // Detectar WebP
+        if (productoActivo.imageData.startsWith('UklGR')) {
+          imageExtension = 'webp';
+        }
+    
+        return `${base64Prefix}${imageExtension};base64, ${productoActivo.imageData}`;
+      };
+    
+
+        return (
+            <div className="admin-product-container">
+                <button onClick={desactivarProducto} id="boton-desactivador">
+                    X
+                </button>
+                <div className="admin-image-container">
+                    {productoActivo && <img src={getImageSource()} alt="imagen del producto" />}
+                </div>
+
+                <section className="producto-info-admin">
+                    <table id="TABLE-INFO">
+                        <tbody>
+                            <tr>
+                                <td className="left-header">Nombre</td>
+                                <td className="right-datacell">
+                                    {isUnderChanges ? <input type="text" defaultValue={productoActivo.name} /> : <p>{productoActivo.name}</p>}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="left-header">Descripcion</td>
+                                <td className="right-datacell">
+                                    {isUnderChanges ? <input type="text" defaultValue={productoActivo.description} /> : <p>{productoActivo.description}</p>}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="left-header">Precio</td>
+                                <td className="right-datacell">
+                                    {isUnderChanges ? <input type="number" defaultValue={productoActivo.price} /> : <p>{productoActivo.price}</p>}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="left-header">Stock</td>
+                                <td className="right-datacell">
+                                    {isUnderChanges ? <input type="number" defaultValue={productoActivo.stock} /> : <p>{productoActivo.stock}</p>}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button id="ACEPTAR-BTN" onClick={handleAceptarBTN}>{isUnderChanges ? "Aceptar" : "Modificar"}</button>
+                    <button id="ELIMINAR-BTN" onClick={handleEliminarProducto}>Eliminar Producto</button>
+                </section>
+            </div>
+        );
     }
-    return(
-      <>
-        {productoActivo ? renderProducto() : renderTable()}
-        {showModal && <NewProdModal onClose={closeModal} />}
-      </>
+
+    function handleAceptarBTN() {
+        if (isUnderChanges) {
+            setIsUnderChanges(false);
+        } else {
+            setIsUnderChanges(true);
+        }
+    }
+
+    return (
+        <>
+            {productoActivo ? renderProducto() : renderTable()}
+            {showModal && <NewProdModal onClose={closeModal} />}
+        </>
     );
 }
